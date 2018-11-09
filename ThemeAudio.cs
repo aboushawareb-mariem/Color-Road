3afdd918c6b1b7d3e239d80ae098ccb03ae30714
aboushawareb-mariem <aboushawareb-mariem@users.noreply.github.com>
2018-11-09 06:21:19 +0000

    Add files via upload

---
+++ ThemeAudio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThemeAudio : MonoBehaviour {
+
+    public AudioClip MusicClip;
+    public AudioSource MusicSource;
+	// Use this for initialization
+	void Start () {
+        MusicSource.clip = MusicClip;
+            MusicSource.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(!MusicSource.isPlaying)
+        {
+            MusicSource.Play();
+        }
+            
+        
+		
+	}
+}
